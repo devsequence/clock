39b0a491ffcfbdd833e671ac8c687b5d60d418d6
Slipak Bohdan <fear3494@gmail.com>
2021-02-10 08:59:03 +0200

    created 404 page

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -82,3 +82,32 @@ $('.read-more').on('click', function () {
     $this.prev().toggleClass('show-more')
 });
 // guarantee item show more
+
+// slick slider about page
+
+$('.about-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
